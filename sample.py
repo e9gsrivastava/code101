050defd776185201a63ee564d7b543a5acf607a1
gaurav srivastava <gaurav.srivastava@enine.school>
2023-12-18 10:02:25 +0530

    blacked and pylnted

--- sample.py
+++ sample.py
@@ -1,5 +1,9 @@
-x = 0
-for i in range(4212, 18912513):
-    x += i
+def home():
+    x = 0
+    for i in range(4212, 18912513):
+        x += i
+    return x
 
-print(x)
+
+if __name__ == "__main__":
+    print(home())
